4e3d716317f96ca90c1503b31f65503afc2ec563
Maicon Gavino <maiconserrao57@gmail.com>
2024-11-12 18:48:31 -0400

    web server with go

--- WebServeGo/main.go
+++ WebServeGo/main.go
@@ -1,9 +1,43 @@
 package main
 
-import(
+import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
-func main(){
-	fmt.Println("Initial Commit")
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	nome := r.FormValue("nome")
+	email := r.FormValue("email")
+	fmt.Fprintf(w, "Nome = %s\n", nome)
+	fmt.Fprintf(w, "Email = %s\n", email)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
