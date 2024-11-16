d610e9133dfa84e0826dcb1dd68ffbd76460919c
Maicon Gavino <maiconserrao57@gmail.com>
2024-11-16 17:14:00 -0400

    atualization

--- ProjectWeather/main.go
+++ ProjectWeather/main.go
@@ -1,20 +1,21 @@
 package main
 
-import(
-	"log"
-	"os"
-	"net/http"
+import (
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/joho/godotenv"
 )
 
-type OpenWeatherResponse struct{
-	Main struct{
+type OpenWeatherResponse struct {
+	Main struct {
 		Temp float64 `json:"temp"`
-	}`json:"main"`
+	} `json:"main"`
 	Name string `json:"name"`
 }
 
@@ -23,17 +24,17 @@ type WeatherResponse struct {
 	Temperature float64
 }
 
-func loadEnv(){
+func loadEnv() {
 	err := godotenv.Load()
-	if err != nil{
+	if err != nil {
 		log.Fatal("Error loading .env file:", err)
 	}
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request){
+func weatherHandler(w http.ResponseWriter, r *http.Request) {
 	city := r.URL.Query().Get("city")
 	if city == "" {
-		city = "Campinas"
+		city = "Porto Velho" // Cidade padrão
 	}
 
 	apiKey := os.Getenv("API_KEY")
@@ -63,77 +64,25 @@ func indexHandler(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
-	// Dados que serão enviados ao template
-	data := WeatherResponse{
+	weatherData := WeatherResponse{
 		City:        openWeather.Name,
 		Temperature: openWeather.Main.Temp,
 	}
 
-	// Renderiza o template com os dados
+	// Renderiza o template com os dados dinâmicos
 	tmpl := template.Must(template.ParseFiles("./template/index.html"))
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Error rendering template", http.StatusInternalServerError)
-	}
+	tmpl.Execute(w, weatherData)
 }
 
-
-// func weatherHandler(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json") // Corrigido o Content-Type
-
-// 	city := r.URL.Query().Get("city")
-// 	if city == "" {
-// 		city = "Porto Velho"
-// 	}
-
-// 	apiKey := os.Getenv("API_KEY")
-// 	if apiKey == "" {
-// 		http.Error(w, "API_KEY not configured", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&appid=%s", city, apiKey)
-
-// 	resp, err := http.Get(url)
-// 	if err != nil {
-// 		http.Error(w, "Error fetching data from API", http.StatusInternalServerError)
-// 		return
-// 	}
-// 	defer resp.Body.Close()
-
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		http.Error(w, "Error reading response", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	var openWeather OpenWeatherResponse
-// 	if err := json.Unmarshal(body, &openWeather); err != nil {
-// 		http.Error(w, "Error decoding JSON", http.StatusInternalServerError)
-// 		return
-// 	}
-// 	response := WeatherResponse{
-// 		City:        openWeather.Name,
-// 		Temperature: openWeather.Main.Temp,
-// 	}
-
-// 	w.WriteHeader(http.StatusOK)
-// 	json.NewEncoder(w).Encode(response)
-// }
-
-
-
-
-func main(){
+func main() {
 	loadEnv()
 
 	fs := http.FileServer(http.Dir("./template/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/", indexHandler)
-	//http.HandleFunc("/weather", weatherHandler)
+	http.HandleFunc("/weather", weatherHandler)
 
-	log.Println("Serve running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080",nil))
-
+	log.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
