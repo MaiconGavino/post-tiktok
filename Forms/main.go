30699d370d724c930592cc843f3ab085b88fe2ac
Maicon Gavino <maiconserrao57@gmail.com>
2024-11-19 11:36:13 -0400

    new version

--- Forms/main.go
+++ Forms/main.go
@@ -3,23 +3,26 @@ package main
 import (
 	"forms/config"
 	"forms/handlers"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
 	config.ConnectDB()
-	defer config.DB.Close()
 
+	// Configura para servir arquivos estáticos
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", handlers.RegistrationFormHandler).Methods("GET")
-	r.HandleFunc("/register", handlers.RegisterUserHandler).Methods("POST")
-	r.HandleFunc("/sucess", handlers.SuccessHandler).Methods("GET")
+	// Rota para renderizar o formulário
+	http.HandleFunc("/", handlers.RegistrationFormHandler)
 
-	log.Println("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	// Rota para registrar o usuário
+	http.HandleFunc("/register", handlers.RegisterUserHandler)
+
+	// Rota para a página de sucesso
+	http.HandleFunc("/success", handlers.SuccessHandler)
+
+	log.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
